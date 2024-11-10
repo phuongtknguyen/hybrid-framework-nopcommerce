215136e9b6fa3b38bd643a68f299f3df712ed068
Nguyen Thi Kieu Phuong <phuongtkn3004@gmail.com>
2024-11-10 22:11:47 +0700

    Add NewCustomerPageObject and user_01_NewCustomer

---
+++ testcases/fpt/mytv/login/User_01_NewCustomer.java
@@ -0,0 +1,15 @@
+package fpt.mytv.login;
+
+import org.testng.annotations.Test;
+
+public class User_01_NewCustomer {
+    @Test
+    public void Customer_01_Empty(){
+
+    }
+
+    @Test
+    public void Customer_02_Invalid_Email(){
+
+    }
+}
